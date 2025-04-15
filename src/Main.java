f1f4e3ec97877f09342c017f362c534b6585fad7
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-04-15 23:21:59 +0530

    demo added for cache

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import models.Cache;
+import models.ThreadSafeCache;
+import strategy.lfu.LFUEvictionPolicy;
+import strategy.lru.LRUEvictionPolicy;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cache<Integer, String> lruCache = new ThreadSafeCache<>(3, new LRUEvictionPolicy<>());
+        Cache<Integer, String> lfuCache = new ThreadSafeCache<>(3, new LFUEvictionPolicy<>());
+
+        lruCache.put(1, "A");
+        lruCache.put(2, "B");
+        lruCache.put(3, "C");
+        lruCache.get(1);
+        lruCache.put(4, "D");
+
+        lfuCache.put(1, "A");
+        lfuCache.put(2, "B");
+        lfuCache.put(3, "C");
+        lfuCache.get(1);
+        lfuCache.get(1);
+        lfuCache.get(2);
+        lfuCache.put(4, "D");
+    }
+}
